0fe62a46801e62f66a3bec62d60a90db64d736dc
Oluwatobi Okunuga <tobi@macbook-pro.home>
2023-06-11 16:05:47 -0400

    added state variables

--- src/App.js
+++ src/App.js
@@ -2,20 +2,50 @@ import "./App.css";
 import Header from "./components/layout/Header/Header";
 import LogoToggle from "./components/layout/LogoToggle/LogoToggle";
 import SearchBar from "./components/SearchBar/SearchBar";
-import Card from "./components/CardList/CardList";
+import CardList from "./components/CardList/CardList";
 import { jobsData } from "./data";
+import { useState } from "react";
 
 function App() {
-	console.log(jobsData);
+	const [searchTerm, setSearchTerm] = useState("");
+	const [searchByLocation, setSearchByLocation] = useState("");
+
+	const handleChange = (e) => {
+		setSearchTerm(e.target.value);
+	};
+
+	const searchedJobDetails = jobsData.filter(
+		(searchedJob) =>
+			searchedJob.company
+				.toLowerCase()
+				.includes(searchTerm.toLowerCase()) ||
+			searchedJob.position
+				.toLowerCase()
+				.includes(searchTerm.toLowerCase())
+	);
+
+	const handleJobLocationChange = (e) => {
+		setSearchByLocation(e.target.value);
+	};
+
+	// const searchedJobByLocation = jobsData.filter((searchedJob) =>
+	// 	searchedJob.location
+	// 		.toLowerCase()
+	// 		.includes(searchByLocation.toLowerCase())
+	// );
+
 	return (
 		<>
 			<Header />
 			<div className="wrapper">
 				<div style={{ position: "relative", top: "-120px" }}>
 					<LogoToggle />
-					<SearchBar />
+					<SearchBar
+						onSearch={handleChange}
+						// onSearchLocation={handleJobLocationChange}
+					/>
 				</div>
-				<Card list={jobsData} />
+				<CardList list={searchedJobDetails} />
 			</div>
 		</>
 	);
